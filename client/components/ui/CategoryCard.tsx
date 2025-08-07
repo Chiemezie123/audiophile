d139aa7ef345700dd4b9da0165d759748b8764ec
Chukwuebuka <chukwuebukaray@gmail.com>
2025-08-07 01:50:12 +0100

    took corrections

--- client/components/ui/CategoryCard.tsx
+++ client/components/ui/CategoryCard.tsx
@@ -1,24 +1,40 @@
 import React from "react";
 import { Button } from "./button";
+import ArrowRight from "@/assets/svg/ArrowRightsvg.svg";
+import Oval from "@/assets/Oval.png";
+import { cn } from "@/lib/utils";
 
 type CategoryCardProps = {
   image: string;
   alt: string;
   title: string;
+  imageClassName?: string;
 };
 
-const CategoryCard = ({ image, alt, title }: CategoryCardProps) => {
+const CategoryCard = ({
+  image,
+  alt,
+  title,
+  imageClassName,
+}: CategoryCardProps) => {
   return (
-    <div>
-      <div className="bg-[#f1f1f1] h-[204px] w-[327px] md:w-[223px] lg:w-[350px] flex flex-col items-center gap-2 mt-20 rounded-[8px] relative">
+    <div className="w-full flex-1">
+      <div className="bg-[#f1f1f1] md:h-[165px] lg:h-[204px] flex flex-col items-center mt-20 rounded-[8px] relative">
         <img
           src={image}
           alt={alt}
-          className="w-[123px] h-[160px] absolute bottom-30"
+          className={cn(` absolute xs:bottom-26 md:bottom-29 lg:bottom-35`, imageClassName)}
         />
-        <div className="mt-25">
-          <h6 className="font-bold">{title}</h6>
-          <Button variant={"tertiary"}>Shop</Button>
+        <img
+          src={Oval.src}
+          alt="Oval"
+          className="absolute xs:bottom-21 md:bottom-25 lg:bottom-31 w-[95px] h-[14px] blur-xl bg-black"
+        />
+        <div className="mt-22 lg:mt-29">
+          <h6 className="md:text-[15px] font-bold">{title}</h6>
+          <Button variant={"tertiary"} rightIcon={<ArrowRight />}>
+            Shop
+          </Button>
         </div>
       </div>
     </div>
--- client/features/Header.tsx
+++ client/features/Header.tsx
@@ -7,11 +7,12 @@ const Header = () => {
   return (
     <div className="bg-[#141414]">
       <div className="xs:max-w-[327px] md:max-w-[689px] lg:max-w-[1110px] mx-auto">
-        <div className="flex items-center justify-between z-1 py-8 border-b-2 border-white border-opacity-20">
+        <div className="flex items-center justify-between py-8 border-b-2 border-[rgba(255,255,255,0.2)] border-opacity-20  ">
           <div className=" flex items-center gap-10">
             <HamburgerIcon className="lg:hidden xs:block" />
-            <LogoIcon />
+            <LogoIcon className="xs:hidden md:block" />
           </div>
+          <LogoIcon className="xs:block md:hidden" />
           <ul className=" xs:hidden md:hidden lg:inline-flex gap-8.5 pr-0.5 justify-center items-start text-xs uppercase text-white ">
             <li className="cursor-pointer hover:text-[var(--color-warm-orange-brown)]">
               Home
